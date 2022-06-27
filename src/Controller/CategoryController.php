93ef2eb01ee79caa8df8c203a27a75c3c27dd701
AnNguyen1321 <107805959+AnNguyen1321@users.noreply.github.com>
2022-06-27 09:27:17 +0700

    Complete and edit CategoryController

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,11 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Form\CategoryType;
-
+use App\Repository\CategoryRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 #[Route("/category")]
 class CategoryController extends AbstractController
